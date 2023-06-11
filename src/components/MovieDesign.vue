845289ea2435b6142b7cf5a65296f9519ba5b7dc
Seongyoon <namo4862@gmail.com>
2023-06-11 20:07:42 +0900

    ADD: Header, Fix

--- src/components/MovieDesign.vue
+++ src/components/MovieDesign.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 defineProps<{
-  result: { // 영화 정보
+  result: {
+    // 영화 정보
     adult: boolean;
     backdrop_path: string;
     genre_ids: number[];
@@ -21,10 +22,34 @@ defineProps<{
 </script>
 
 <template>
-  <section>
-    <p>{{ result.title }}</p>
-    <img :src="image_path + result.poster_path" :alt="result.title">
+  <section class="movie">
+    <picture class="thumb">
+      <img :src="image_path + result.poster_path" :alt="result.title" />
+    </picture>
+    <div class="info">
+      <h1 class="title">{{ result.title }}</h1>
+      <p class="desc">{{ result.overview }}</p>
+    </div>
   </section>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.movie {
+  box-sizing: border-box;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  column-gap: 20px;
+  color: white;
+  .thumb {
+    display: block;
+    width: 100%;
+    border-radius: 8px;
+    overflow: hidden;
+    img {
+      max-width: 100%;
+      display: block;
+    }
+  }
+}
+</style>
---
+++ src/layout/MainHeader.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+const routerList = ref([
+  {
+    name: 'MOVIE',
+    path: '/movie',
+  },
+  {
+    name: 'DRAMA',
+    path: '/drama',
+  },
+]);
+</script>
+
+<template>
+  <header id="Header">
+    <RouterLink to="/" class="main">HOME</RouterLink>
+    <nav class="navigation">
+      <RouterLink
+        :to="items.path"
+        :key="index"
+        v-for="(items, index) in routerList"
+      >
+        {{ items.name }}
+      </RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+#Header {
+  position: sticky;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 20px;
+  box-sizing: border-box;
+  .main {
+    font-size: 24px;
+    font-weight: 600;
+  }
+}
+</style>
